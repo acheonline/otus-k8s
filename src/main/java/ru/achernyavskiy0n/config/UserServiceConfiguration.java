33a5198cdb46e7949434b1bcd143e891d95b52dd
AntonChernyavskiy <vtb4058290@region.vtb.ru>
2022-02-25 07:20:56 +0300

    clean up

--- src/main/java/ru/achernyavskiy0n/config/UserServiceConfiguration.java
+++
@@ -1,9 +0,0 @@
-package ru.achernyavskiy0n.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
-
-@Configuration
-@Import(PersistenceConfiguration.class)
-public class UserServiceConfiguration {
-}
